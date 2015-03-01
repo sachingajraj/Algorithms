88193bce7bd7a6bf10f02d12ebc9177381fd0c35
sachingajraj <sachingajraj@gmail.com>
2015-03-02 04:04:09 +0530

    added longest increasing subsequence

--- src/ArrayQuestions.java
+++ src/ArrayQuestions.java
@@ -1,5 +1,5 @@
 public class ArrayQuestions {
-	
+
 	public int[] sort012(int[] arr) {
 		int i = 0;
 		int j = 0;
@@ -31,41 +31,74 @@ public class ArrayQuestions {
 
 		int i = 0, j = 0;
 		for (int k = 0; k < res.length; k++) {
-			if(i < arr1.length){
-				res[k] = (arr1[i] < arr2[j])?arr1[i]:arr2[j];
+			if (i < arr1.length) {
+				res[k] = (arr1[i] < arr2[j]) ? arr1[i] : arr2[j];
 				i++;
-			}else{
+			} else {
 				res[k] = arr2[j];
 				j++;
 			}
 		}
 		return res;
 	}
-	
-	// Returns the count of ways we can sum  S[0...m-1] coins to get sum n
-	public int count( int S[], int m, int n )
-	{
-	    // If n is 0 then there is 1 solution (do not include any coin)
-	    if (n == 0)
-	        return 1;
-	     
-	    // If n is less than 0 then no solution exists
-	    if (n < 0)
-	        return 0;
-	 
-	    // If there are no coins and n is greater than 0, then no solution exist
-	    if (m <=0 && n >= 1)
-	        return 0;
-	 
-	    // count is sum of solutions (i) including S[m-1] (ii) excluding S[m-1]
-	    return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
+
+	// Returns the count of ways we can sum S[0...m-1] coins to get sum n
+	public int count(int S[], int m, int n) {
+		// If n is 0 then there is 1 solution (do not include any coin)
+		if (n == 0)
+			return 1;
+
+		// If n is less than 0 then no solution exists
+		if (n < 0)
+			return 0;
+
+		// If there are no coins and n is greater than 0, then no solution exist
+		if (m <= 0 && n >= 1)
+			return 0;
+
+		// count is sum of solutions (i) including S[m-1] (ii) excluding S[m-1]
+		return count(S, m - 1, n) + count(S, m, n - S[m - 1]);
 	}
-	 
-	
-	// print array 
-	public void printArray(int[] arr){
-		for (int i : arr){
-			System.out.println(i + " ");
+
+	// LIS : Longest increasing subsequence
+
+	public int lis(int[] arr) {
+		int[] lisArr = new int[arr.length];
+		int i, max = 0;
+		for (i = 0; i < arr.length; i++) {
+			lisArr[i] = 1;
+		}
+
+		for (i = 1; i < arr.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j] && lisArr[i] < lisArr[j] + 1) {
+					lisArr[i] = 1 + lisArr[j];
+				}
+			}
+		}
+
+		for (i = 1; i < arr.length; i++) {
+			if (max < lisArr[i])
+				max = lisArr[i];
+		}
+		// printArray(arr);
+		// printArray(lisArr);
+
+		int tmp = max;
+		for (i = arr.length - 1; i >= 0; i--) {
+			if (lisArr[i] == tmp) {
+				System.out.print(arr[i] + " ");
+				tmp--;
+			}
+		}
+		System.out.println();
+		return max;
+	}
+
+	// print array
+	public void printArray(int[] arr) {
+		for (int i : arr) {
+			System.out.print(i + " ");
 		}
 		System.out.println();
 	}
--- src/Driver.java
+++ src/Driver.java
@@ -2,51 +2,40 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/*
-		int[] arr = {5,2,1,3,4};
-		SortQuestions s = new SortQuestions(arr);
-		s.quicksort();
-		s.printArray();
-		*/
-		
-		/*
-		StringQuestions sq = new StringQuestions();
-		sq.printAllStringPermutations(new StringBuffer("aba"), 0);
-		*/
-		
-		/*
-		ArrayQuestions aq = new ArrayQuestions();
-		int[] arr = {2,1,0,0,2,1,0,1,1};
-		int[] res = aq.sort012(arr);
-		for (int i :res){
-			System.out.print(i + " ");
-		}
-		*/
-		
-		/*
-		StringQuestions sq = new StringQuestions();
-		sq.longestPalindrome("abaaba");
-		*/
-		
-		/*
-		int[] arr1 = {3,5,10};
-		int[] arr2 = {1,3,6,9};
-		ArrayQuestions aq = new ArrayQuestions();
-		//aq.printArray(aq.merge(arr1, arr2));
-		System.out.println(aq.count(arr1, 3, 20));
-		*/
-		
-		LinkedList ll = new LinkedList(2);
-		ll.insert(3);
-		ll.insert(20);
-		ll.insert(12);
-		ll.printList();
-		
-		
-		LinkedListQuestion lq = new LinkedListQuestion();
-		lq.reverseLinkedListLoop(ll);
-	
-	}
 
+		/*
+		 * int[] arr = {5,2,1,3,4}; SortQuestions s = new SortQuestions(arr);
+		 * s.quicksort(); s.printArray();
+		 */
+
+		/*
+		 * StringQuestions sq = new StringQuestions();
+		 * sq.printAllStringPermutations(new StringBuffer("aba"), 0);
+		 */
+
+		/*
+		 * ArrayQuestions aq = new ArrayQuestions(); int[] arr =
+		 * {2,1,0,0,2,1,0,1,1}; int[] res = aq.sort012(arr); for (int i :res){
+		 * System.out.print(i + " "); }
+		 */
+
+		/*
+		 * StringQuestions sq = new StringQuestions();
+		 * sq.longestPalindrome("abaaba");
+		 */
+
+		int[] arr1 = { 3, 5, 10, -1, 50 };
+		int[] arr2 = {-3, -1, -3, -6, -9 };
+		ArrayQuestions aq = new ArrayQuestions();
+		// aq.printArray(aq.merge(arr1, arr2));
+		System.out.println(aq.lis(arr2));
+
+		/*
+		 * LinkedList ll = new LinkedList(2); ll.insert(3); ll.insert(20);
+		 * ll.insert(12); ll.printList();
+		 * 
+		 * LinkedListQuestion lq = new LinkedListQuestion();
+		 * lq.reverseLinkedListLoop(ll);
+		 */
+	}
 }
